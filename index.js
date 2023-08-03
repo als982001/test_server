fd211b055cfff41a060ce685071a73514dce345f
주재민 <als982001@naver.com>
2023-08-03 21:26:17 +0900

    Feat: key 확인 코드 추가

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const database = "areahunt";
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
+console.log(`testKey = ${process.env.testKey}`);
+
 app.listen(PORT, handleListening);
